941cc8e87a8ff6a5306f795bbeacf7273df7cc33
Youssef El Saadany <youssef.saadany@gmail.com>
2020-09-29 23:53:39 +0200

    Added PasswordFile class
    object representation of a 'password file', it holds multiple accounts
    and the logic necessary to manipulate and save it to disk

---
+++ PasswordFile.py
@@ -0,0 +1,57 @@
+from Account import Account
+import pickle
+
+
+class PasswordFile:
+    def __init__(self, master_key):
+        self.master_key = master_key
+        self.accounts = {}
+
+    def get_accounts(self):
+        return self.accounts.keys()
+
+    def get_account_details(self, account_key):
+        if account_key in self.accounts:
+            return (account_key, self.accounts[account_key].details)
+
+        raise ValueError("This service does not exist.")
+
+    def add_account(self, key):
+        self.accounts[key] = Account(key)
+
+    def remove_account(self, key):
+        self.accounts.pop(key)
+
+    def save_to_file(self, file_name="password_file.pswd"):
+        with open(file_name, "wb") as file:
+            pickle.dump(self, file)
+
+    def menu(self):
+        running = True
+
+        while running:
+            print("Welcome to your Password File")
+            print("1. Get credentials")
+            print("2. Add an account")
+            print("3. Remove an account")
+            print("4. Exit")
+            choice = int(input("Your choice: "))
+
+            if choice == 1:
+                account = self.get_account_details(input("Service: "))
+                print(account)
+
+            elif choice == 2:
+                self.add_account(input("Service: "))
+
+            elif choice == 3:
+                self.remove_account(input("Service: "))
+
+            elif choice == 4:
+                self.save_to_file()
+                running = False
+
+            else:
+                print("Please choose a valid option.")
+
+            self.save_to_file()
