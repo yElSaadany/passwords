905bacf0556cbeaa54416e48908699b2140bbe1f
Youssef El Saadany <youssef.saadany@gmail.com>
2020-10-03 18:43:40 +0200

    Added most important crypto functions
    write_key() generates a symmetric key and writes it to a file,
    load_key() takes a key file and returns the key in bytes,
    encrypt/decrypt_file() do what they say

--- crypto.py
+++ crypto.py
@@ -1,9 +1,38 @@
 from cryptography.fernet import Fernet
 
 
-def write_key():
+def write_key(key_name: str = "key") -> str:
     key = Fernet.generate_key()
 
-    with open("key.key", "wb") as key_file:
+    with open(f"{key_name}.key", "wb") as key_file:
         key_file.write(key)
 
+    return f"{key_name}.key"
+
+
+def load_key(path: str) -> bytes:
+    return open(path, "rb").read()
+
+
+def encrypt_file(file_path: str, key: bytes) -> None:
+    f = Fernet(key)
+
+    with open(file_path, "rb") as file:
+        file_data = file.read()
+
+    encrypted_file = f.encrypt(file_data)
+
+    with open(file_path, "wb") as file:
+        file.write(encrypted_file)
+
+
+def decrypt_file(file_path: str, key: bytes) -> None:
+    f = Fernet(key)
+
+    with open(file_path, "rb") as file:
+        encrypted = file.read()
+
+    decrypted = f.decrypt(encrypted)
+
+    with open(file_path, "wb") as file:
+        file.write(decrypted)
